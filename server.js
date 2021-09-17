561f34ee1286e92bbc4a6b9b5f3ce87aea6baebc
Steven Quintana <steve.e.quint@gmail.com>
2021-09-17 12:53:44 -0700

    Sync sequelize db then turn on server

--- server.js
+++ server.js
@@ -9,4 +9,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(require('./routes'))
 
 // sync sequelize models to the database, then turn on the server
-app.listen(process.env.PORT || 3000)
+require('./db')
+  .sync()
+  .then(() => app.listen(process.env.PORT || 3000))
