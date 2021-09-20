aae40ce5a18ad5ee86f8d3ba8d471a114f96f529
Steven Quintana <steve.e.quint@gmail.com>
2021-09-20 12:46:22 -0700

    Delete validator lib and reference in object

--- models/Product.js
+++ models/Product.js
@@ -24,17 +24,13 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
-      validate: {
-        isDecimal: true
-      }
+      isDecimal: true
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
-      validate: {
-        isNumeric: true
-      }
+      isNumeric: true
     },
     category_id: {
       type: DataTypes.INTEGER,
