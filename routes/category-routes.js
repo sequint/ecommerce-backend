429273974b4f9d413c25841d947c20ae02b337a7
Steven Quintana <steve.e.quint@gmail.com>
2021-09-20 08:22:55 -0700

    Delete foreign key import

--- routes/category-routes.js
+++ routes/category-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router()
-const { FOREIGNKEYS } = require('sequelize/types/lib/query-types')
 const { Category, Product } = require('../models')
 
 // The `/api/categories` endpoint
