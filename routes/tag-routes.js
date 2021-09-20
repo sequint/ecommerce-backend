7d5a89320bf472ede7da16c184421ec95de2786e
Steven Quintana <steve.e.quint@gmail.com>
2021-09-20 12:52:34 -0700

    Create both get functions and delete

--- routes/tag-routes.js
+++ routes/tag-routes.js
@@ -6,12 +6,17 @@ const { Tag, Product, ProductTag } = require('../models')
 router.get('/tags', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  
+  Tag.findAll({ include: Product })
+    .then(tags => res.json(tags))
+    .catch(err => console.log(err))
 })
 
 router.get('/tags/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findAll({ include: Product, where: { id: req.params.id } })
+    .then(category => res.json(category))
+    .catch(err => console.log(err))
 })
 
 router.post('/tags', (req, res) => {
@@ -24,6 +29,9 @@ router.put('/tags/:id', (req, res) => {
 
 router.delete('/tags/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.update(req.body, {
+    where: { id: req.params.id }
+  })
 })
 
 module.exports = router
