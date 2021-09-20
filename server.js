ed1a45d23a72189b3178b819acc68c04571f2616
Steven Quintana <steve.e.quint@gmail.com>
2021-09-20 11:23:38 -0700

    Switch sync function to sequelize

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 // import sequelize connection
-const { Sequelize } = require('sequelize')
+const sequelize = require('./config/connection')
 
 const app = express()
 
@@ -10,6 +10,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(require('./routes'))
 
 // sync sequelize models to the database, then turn on the server
-require('./db').sync()
+sequelize.sync({})
   .then(() => app.listen(process.env.PORT || 3000))
   .catch(err => console.log(err))
