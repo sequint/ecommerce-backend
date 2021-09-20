4b0ec34d83194fe6381e75a82343908e3bfc3701
Steven Quintana <steve.e.quint@gmail.com>
2021-09-20 11:24:05 -0700

    Change url to custom local

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.JAWSDB_URL ? process.env.JAWSDB_URL : process.env.LOCALDB_URL)
+const sequelize = new Sequelize(process.env.JAWSDB_URL || `mysql://root:${process.env.PASSWORD}@localhost:3306/ecommerce_db`)
 
 module.exports = sequelize
