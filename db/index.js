025d1072cefa0be06f1170466f0514f76a1d038e
Steven Quintana <steve.e.quint@gmail.com>
2021-09-20 11:24:28 -0700

    Delete db index since config connection is same

--- db/index.js
+++
@@ -1,6 +0,0 @@
-const { Sequelize } = require('sequelize')
-require('dotenv').config()
-
-const sequelize = new Sequelize(process.env.JAWSDB_URL || `mysql://root:${process.env.PASSWORD}@localhost:3306/ecommerce_db`)
-
-module.exports = sequelize
