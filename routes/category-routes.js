f473bf9c1901fdaee37052b557a8371e37daebfd
Steven Quintana <steve.e.quint@gmail.com>
2021-09-20 11:27:23 -0700

    Delete model properties from find objects

--- routes/category-routes.js
+++ routes/category-routes.js
@@ -7,9 +7,7 @@ router.get('/categories', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
-    include: [{
-      model: Product
-    }]
+    include: Product
   })
     .then(categories => res.json(categories))
     .catch(err => console.log(err))
@@ -18,12 +16,7 @@ router.get('/categories', (req, res) => {
 router.get('/categories/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  Category.findAll({
-    include: [{
-      model: Product,
-      where: { id: req.params.id}
-    }]
-  })
+  Category.findAll({ include: Product, where: { id: req.params.id} })
     .then(category => res.json(category))
     .catch(err => console.log(err))
 })
